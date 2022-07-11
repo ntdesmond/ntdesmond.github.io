4b7ecd23e1b3cbf920adc88cbb34e0e6e758b31f
Desmond Ainotohi <29694249+ntdesmond@users.noreply.github.com>
2022-07-11 19:28:03 +0300

    Add content to the home page

---
+++ src/components/typography/Headings.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const PageHeading = styled.h1`
+  font-size: 3em;
+  font-weight: bold;
+  font-style: normal;
+`;
+
+export const SectionHeading = styled.h2`
+  font-size: 2em;
+  font-weight: bold;
+  font-style: normal;
+`;
+
+export const InTextHeading = styled.h3`
+  font-size: 1.5em;
+  font-weight: bold;
+  font-style: normal;
+`;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,29 @@
+import { Link } from "react-router-dom";
 import Layout from "../components/layout/Layout";
+import { InTextHeading } from "../components/typography/Headings";
 
-const Home = () => <Layout>Hello, it's me</Layout>;
+const Home = () => (
+  <Layout>
+    <p>
+      Hello, <code>ntdesmond</code> here.
+    </p>
+    <InTextHeading>Who?</InTextHeading>
+    <p>
+      <code>ntdesmond</code>, usually pronounced as{" "}
+      <i>NT Desmond (an tee desmond)</i> is my nickname.
+    </p>
+    <p>
+      My real name is <b>Vladislav Safonov</b>, and, as of 2022, I'm a student
+      of <a href="https://innopolis.university/">Innopolis University</a>.
+    </p>
+    <InTextHeading>So?</InTextHeading>
+    <p>
+      Originally this site is meant to hold my{" "}
+      <Link to="/cv">"interactive" CV</Link>, written in React, so that it works
+      as an example of my work.
+    </p>
+    <p>Probably, I will add some more stuff here, we'll see.</p>
+  </Layout>
+);
 
 export default Home;
