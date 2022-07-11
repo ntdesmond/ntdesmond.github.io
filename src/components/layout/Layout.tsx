0e21d980535748b5b8f56171ecc30f066f886db4
Desmond Ainotohi <29694249+ntdesmond@users.noreply.github.com>
2022-07-11 18:54:53 +0300

    Reduce width of content

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,11 +3,14 @@ import styled from "styled-components";
 import Header from "./header/Header";
 
 const StyledLayout = styled.div`
+  transition: padding 0.2s;
   height: 100%;
   display: grid;
   grid-template:
     "header" 2em
     "main" auto;
+  max-width: 800px;
+  margin: 0 auto;
 `;
 
 const StyledMain = styled.section`
