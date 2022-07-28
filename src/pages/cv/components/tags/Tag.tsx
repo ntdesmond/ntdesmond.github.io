922fc1d6eb936661db7b7f9447d231637e57ba69
Vladislav Safonov <29694249+ntdesmond@users.noreply.github.com>
2022-07-28 19:35:10 +0300

    Add print mode that disables tag hrefs

--- src/pages/cv/components/tags/Tag.tsx
+++ src/pages/cv/components/tags/Tag.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import TagsContext from '../../contexts/TagsContext';
 
 const StyledTag = styled(Link)`
+  color: #0066cc;
   font-weight: 500;
   text-decoration: underline dotted;
 `;
@@ -22,6 +23,8 @@ const Tag = (props: { slug: string; name?: string; register?: boolean }) => {
   const { hash, search } = useLocation();
   const { pushTag } = useContext(TagsContext);
 
+  const isPrintMode = new URLSearchParams(search).get('print') !== null;
+
   const target = `#${encodeURI(props.slug)}`;
 
   useEffect(() => {
@@ -29,6 +32,10 @@ const Tag = (props: { slug: string; name?: string; register?: boolean }) => {
       pushTag(props.slug);
     }
   }, []);
+
+  if (isPrintMode) {
+    return <StyledTag as="span">{props.name || props.slug}</StyledTag>;
+  }
 
   return decodeURI(hash.slice(1)) === props.slug ? (
     <StyledSelectedTag to={target}>
