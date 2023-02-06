881cbadc93390b750426ca06cb81d9bbb1e7f7b5
Vladislav Safonov <29694249+ntdesmond@users.noreply.github.com>
2023-02-06 17:36:56 +0300

    Set empty publicPath for local html to work

--- webpack.common.js
+++ webpack.common.js
@@ -66,7 +66,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'build'),
     filename: '[name].[fullhash].bundle.js',
-    publicPath: '/',
+    publicPath: '',
     clean: true,
   },
 };
